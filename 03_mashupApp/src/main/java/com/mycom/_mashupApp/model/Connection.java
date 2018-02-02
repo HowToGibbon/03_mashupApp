c630d03d48b26610af10b5856650ec3153e59bc1
DESKTOP-L8O5CD0\Nick Wallner <nick.wallner.nick@gmail.com>
2018-02-02 21:26:17 +0100

    model classes

---
+++ 03_mashupApp/src/main/java/com/mycom/_mashupApp/model/Connection.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+/**
+ * Created by 5ia15nipetkovic on 01.02.2018.
+ */
+public class Connection {
+    private LocalTime departure;
+    private LocalTime arrival;
+    private String platform;
+    private String duration;
+    private ArrayList<String> products;
+
+    public LocalTime getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(LocalTime departure) {
+        this.departure = departure;
+    }
+
+    public LocalTime getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(LocalTime arrival) {
+        this.arrival = arrival;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public ArrayList<String> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<String> products) {
+        this.products = products;
+    }
+}
---
+++ 03_mashupApp/src/main/java/com/mycom/_mashupApp/model/Weather.java
@@ -0,0 +1,34 @@
+package com.company;
+
+/**
+ * Created by 5ia15nipetkovic on 01.02.2018.
+ */
+public class Weather {
+    private String mainTitle;
+    private String description;
+    private double temp;
+
+    public String getMainTitle() {
+        return mainTitle;
+    }
+
+    public void setMainTitle(String mainTitle) {
+        this.mainTitle = mainTitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+}
